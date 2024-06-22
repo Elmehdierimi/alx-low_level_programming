b3ec953b06e685a4c81a085ea12155d397be71f8
elmehdi erimi <elmehdierimi@gmail.com>
2024-06-21 17:35:23 -0700

    game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * a program that prints the alphabet in lowercase
- * followed by a new line
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
