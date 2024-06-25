718aa071a59583bb4aee11f9ecafd14c82a40c5f
elmehdi erimi <elmehdierimi@gmail.com>
2024-06-25 13:03:36 -0700

    dede

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase followed by a new line
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
