cd1445a15bce3288b90f24f941c6b095a68f1759
elmehdi erimi <elmehdierimi@gmail.com>
2024-06-25 13:26:47 -0700

    dedefde

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,9 @@ void print_alphabet_x10(void)
 {
 int a;
 char b;
-for (a = 0, a <= 10, a++)
+for (a = 0; a < 10; a++)
 {
-for (b = 'a', b <= 'z', b++)
+for (b = 'a'; b <= 'z'; b++)
 {
 _putchar(b);
 }
