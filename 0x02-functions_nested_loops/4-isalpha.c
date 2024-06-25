ea7dea328709066d856c9502211b840a8070e413
elmehdi erimi <elmehdierimi@gmail.com>
2024-06-25 14:48:46 -0700

    fff

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - Checks if a character is alphabetic.
- *
+ * @a: The character to be checked.
  * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
 int _isalpha(int a)
