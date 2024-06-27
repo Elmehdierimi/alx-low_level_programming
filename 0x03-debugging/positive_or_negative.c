e83bacc79f5d6433af741201bc580b68e5efde48
elmehdi erimi <elmehdierimi@gmail.com>
2024-06-27 06:04:07 -0700

    dewfr

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,10 +3,11 @@
 
 void positive_or_negative(int i)
 {
-    if (i > 0)
-        printf("%d is positive\n", i);
-    else if (i == 0)
-        printf("%d is zero\n", i);
-    else
-        printf("%d is negative\n", i);
+	/* Check if the number is positive, negative, or zero */
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
 }
