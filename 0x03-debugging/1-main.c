c4a9847aa08b7d7a70c5ec4258dc014a3c04d1d0
elmehdi erimi <elmehdierimi@gmail.com>
2024-06-27 07:36:10 -0700

    eded

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message indicating an incoming infinite loop,
+ *              then avoids the loop and prints a success message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    printf("Infinite loop incoming :(\n");
+
+    i = 0;
+
+    /* Commented out the infinite loop to avoid the issue */
+    /*
+     * while (i < 10)
+     * {
+     *     putchar(i);
+     * }
+     */
+
+    printf("Infinite loop avoided! \\o/\n");
+
+    return (0);
+}
