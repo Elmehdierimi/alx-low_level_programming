f961e21dbeaca6d8730c79e67a81d81a24701535
elmehdi erimi <elmehdierimi@gmail.com>
2024-06-27 16:17:30 -0700

    df

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0 to 9
- * Return: void 
- */
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
 
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = 0 ; i < 10 ; i++)
-		_putchar(i + '0');
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
 	_putchar('\n');
 }
