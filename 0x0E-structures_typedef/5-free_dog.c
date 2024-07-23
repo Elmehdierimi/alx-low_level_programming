91ca755e7518eb04fbd53d6870723879f52c6d9d
elmehdi erimi <elmehdierimi@gmail.com>
2024-07-23 07:40:04 -0700

    dssda

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib>
+
+/**
+ * free_dog - frees yo dawgs
+ * @d: yo dawg
+ *
+ * Return: void.
+ */
+void free_dog(dog_t *d)
+{
+	if(d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
