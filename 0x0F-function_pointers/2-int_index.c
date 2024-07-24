191d724f14b5ea495f82a1f0a7d548e45d0f2eeb
elmehdi erimi <elmehdierimi@gmail.com>
2024-07-24 10:42:17 -0700

    dydy

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for intù@array: the int array
+ * @size: the array size
+ * @cmp: the compare function
+ *
+ * Return: the integer index
+ */
+int int_index(int *array, int size, int (*cmp)(int));
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+	return (-1);
+}
