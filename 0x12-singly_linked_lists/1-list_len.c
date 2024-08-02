86385f7b400e36dabf3f6f2517d4ff3b0fdf6274
Elmehdi <elmehdierimi@gmail.com>
2024-08-02 09:45:03 -0700

    geufu

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,7 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * list_len - find the length of a list
@@ -7,12 +10,12 @@
  */
 size_t list_len(const list_t *h)
 {
-        size_t nodes = 0;
+	size_t nodes = 0;
 
-        while (h != NULL)
-        {
-                h = h->next;
-                nodes++;
-        }
-        return (nodes);
+	while (h != NULL)
+	{
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
 }
