08ccd11cede5f1f627832d2857bbf6bb3f479750
Elmehdi <elmehdierimi@gmail.com>
2024-08-01 17:04:08 -0700

    gyefg

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,21 +8,21 @@
 
 size_t print_list(const list_t *h)
 {
-        size_t nodes = 0;
+	size_t nodes = 0;
 
-        while (h != NULL)
-        {
-                if (h->str == NULL)
-                {
-                        printf("[%d] %s\n", 0, "(nil)");
-                }
-                else
-                {
-                        printf("[%d] %s\n", h->len, h->str);
-                }
-                h = h->next;
-                nodes++;
-        }
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		nodes++;
+	}
 
-        return (nodes);
+	return (nodes);
 }
